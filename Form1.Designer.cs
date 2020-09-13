363623692e9e855ea4eb529b4819ef4ddd7fd27f
Amritpal Singh <amritbhangu650@gmail.com>
2020-09-14 00:02:13 +1200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace DSED_01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnTry = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnImune = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnExit = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnLoad
+            // 
+            resources.ApplyResources(this.btnLoad, "btnLoad");
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // btnTry
+            // 
+            resources.ApplyResources(this.btnTry, "btnTry");
+            this.btnTry.Name = "btnTry";
+            this.btnTry.UseVisualStyleBackColor = true;
+            this.btnTry.Click += new System.EventHandler(this.btnTry_Click);
+            // 
+            // btnReset
+            // 
+            resources.ApplyResources(this.btnReset, "btnReset");
+            this.btnReset.Name = "btnReset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnImune
+            // 
+            resources.ApplyResources(this.btnImune, "btnImune");
+            this.btnImune.Name = "btnImune";
+            this.btnImune.UseVisualStyleBackColor = true;
+            this.btnImune.Click += new System.EventHandler(this.btnImune_Click);
+            // 
+            // pictureBox1
+            // 
+            resources.ApplyResources(this.pictureBox1, "pictureBox1");
+            this.pictureBox1.BackColor = System.Drawing.Color.Green;
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblOutput
+            // 
+            resources.ApplyResources(this.lblOutput, "lblOutput");
+            this.lblOutput.BackColor = System.Drawing.Color.Silver;
+            this.lblOutput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblOutput.ForeColor = System.Drawing.Color.White;
+            this.lblOutput.Name = "lblOutput";
+            // 
+            // lblInfo
+            // 
+            resources.ApplyResources(this.lblInfo, "lblInfo");
+            this.lblInfo.BackColor = System.Drawing.Color.Silver;
+            this.lblInfo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblInfo.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblInfo.Name = "lblInfo";
+            // 
+            // panel1
+            // 
+            resources.ApplyResources(this.panel1, "panel1");
+            this.panel1.BackColor = System.Drawing.Color.DarkGreen;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.lblInfo);
+            this.panel1.Controls.Add(this.lblOutput);
+            this.panel1.Name = "panel1";
+            // 
+            // panel2
+            // 
+            resources.ApplyResources(this.panel2, "panel2");
+            this.panel2.BackColor = System.Drawing.Color.DarkGreen;
+            this.panel2.Controls.Add(this.btnExit);
+            this.panel2.Controls.Add(this.btnImune);
+            this.panel2.Controls.Add(this.btnReset);
+            this.panel2.Controls.Add(this.btnTry);
+            this.panel2.Controls.Add(this.btnLoad);
+            this.panel2.Name = "panel2";
+            // 
+            // btnExit
+            // 
+            resources.ApplyResources(this.btnExit, "btnExit");
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Name = "btnExit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.CancelButton = this.btnExit;
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnTry;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnImune;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.Label lblInfo;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+//start of project
+namespace DSED_01
+{
+    // start of form
+    public partial class Form1 : Form
+    {
+        // declares integers
+        private int Bomb { get; set; } = 0;
+        private int Trigger { get; set; } = 0;
+        // creates counter for score tracking
+        private int Counter { get; set; } = 0;
+        // creates number for shield when shield = 2 turns off shield
+        private int SheildCounter { get; set; } = 0;
+
+        //Link to method Gameplay
+        private GamePlay gamePlay = new GamePlay();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // button to start game
+
+        // creates a new list of numbers already used
+        private List<int> SuccessfulTries = new List<int>();
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            //loads internal method
+            Load();
+        }
+
+        /// <summary>
+        /// Local method to load game on button click
+        /// </summary>
+
+        private new void Load()
+        {
+            //disables / enables buttons
+            btnLoad.Enabled = false;
+            btnTry.Enabled = true;
+            btnImune.Enabled = true;
+            //! Bomb is the value you get when attempting
+
+            //! Trigger is the number set to cause a fail state when bomb = trigger
+            Trigger = gamePlay.RandomGen();
+
+            // displays when game has mode has started
+            lblInfo.Text = "Bomb Armed";
+            lblInfo.BackColor = System.Drawing.Color.Green;
+        }
+
+        // make attempt button
+        private void btnTry_Click(object sender, EventArgs e)
+        {
+            // checks if there are dupe numbers in list
+            Bomb = gamePlay.DuplicateFilter(SuccessfulTries, Bomb);
+            Bomb = gamePlay.RandomGen();
+
+            // checks if the value of Bomb matches the value of Trigger
+            if (Bomb == Trigger)
+            {
+                //!triggers game over state
+                lblOutput.Text = "You Lose";
+                lblOutput.BackColor = System.Drawing.Color.Red;
+                lblInfo.BackColor = System.Drawing.Color.Red;
+                lblInfo.Text = "The Bomb Exploded";
+                btnTry.Text = "Game is Over";
+                //disables buttons
+                btnTry.Enabled = false;
+                btnLoad.Enabled = false;
+                btnImune.Enabled = false;
+                // Plays sound effect
+                System.Media.SoundPlayer player = new System.Media.SoundPlayer(Properties.Resources.Explosion);
+                player.Play();
+            }
+            // if the values do not match you win
+            else
+            {
+                //! triggers a successful attempt state
+                lblOutput.Text = "Success";
+                SuccessfulTries.Add(Bomb);
+                lblInfo.Text = "You disarmed part of the bomb";
+                //adds 1 to the counter
+                Counter++;
+            }
+            // if counter = 5 you win
+            if (Counter == 5)
+            {
+                lblInfo.Text = ("You Win You Defused all 5 parts of the bomb");
+                //disables buttons
+                btnTry.Enabled = false;
+                btnLoad.Enabled = false;
+                btnImune.Enabled = false;
+                // Plays sound effect
+                System.Media.SoundPlayer player = new System.Media.SoundPlayer(Properties.Resources.winner);
+                player.Play();
+            }
+        }
+
+        //button to reset game by reseting application
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            // resets .exe file
+            Application.Restart();
+        }
+
+        /// <summary>
+        /// button for shield option
+        private void btnImune_Click(object sender, EventArgs e)
+        {
+            Bomb = gamePlay.RandomGen();
+            Bomb = gamePlay.DuplicateFilter(SuccessfulTries, Bomb);
+
+            /// uses shield to not die from explosion causes a win state
+
+            // checks if value of Bomb matches Value of Trigger
+            if (Bomb == Trigger)
+            {
+                //!triggers successful state
+                lblOutput.Text = "You Win";
+                lblOutput.BackColor = System.Drawing.Color.Green;
+                lblInfo.BackColor = System.Drawing.Color.Green;
+                lblInfo.Text = "The Bomb Exploded But you survived";
+                btnTry.Text = "You Win";
+                //disables buttons
+                btnTry.Enabled = false;
+                btnLoad.Enabled = false;
+                btnImune.Enabled = false;
+                // Plays sound effect
+                System.Media.SoundPlayer player = new System.Media.SoundPlayer(Properties.Resources.winner);
+                player.Play();
+            }
+            else
+
+            {
+                //! triggers a successful attempt state
+                lblOutput.Text = "Success";
+                SuccessfulTries.Add(Bomb);
+                lblInfo.Text = "You disarmed part of the bomb";
+                //adds 1 to the counter
+                Counter++;
+            }
+
+            if (Counter == 5)
+            {
+                lblInfo.Text = ("You Win You Defused all 5 parts of the bomb");
+                //disables buttons
+                btnTry.Enabled = false;
+                btnLoad.Enabled = false;
+                btnImune.Enabled = false;
+                // Plays sound effect
+                System.Media.SoundPlayer player = new System.Media.SoundPlayer(Properties.Resources.winner);
+                player.Play();
+            }
+
+            // disables shields after two uses
+            // checks if the value of counter is = to 2
+            if (SheildCounter == 2)
+            {
+                //disables buttons
+                btnImune.Enabled = false;
+                btnImune.Text = "Out of shields";
+            }
+            SheildCounter++;
+        }
+
+        // button to exit game
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
